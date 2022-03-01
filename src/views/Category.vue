e5da3a2831aa511df57378f768698f9681cf5af8
George <gsemmail@gmail.com>
2022-03-01 19:42:24 +0300

    start of working with sql

---
+++ src/views/Category.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<h1 class="title">
+			{{ subjectsRu[subjects.indexOf($route.params.category)] }}
+		</h1>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+    font-size: 5em;
+    text-transform: capitalize;
+    margin-left: 50px;
+    margin-top: 100px;
+}
+</style>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+
+export default class Category extends Vue {
+    name = this.$route.params.category
+    subjects = ['geography','math','IT']
+    subjectsRu = ['география','математика','информатика']
+    
+    changeRoute(route) {
+    	this.$router.push(route).catch(error => {});
+    }
+    
+    created() {
+    	if (this.subjects.includes(this.name)) {
+    		console.log('everything\'s fine');
+    	} else {
+    		console.log('everything\'s not fine');
+    		this.changeRoute('/notFound');
+    	}
+    }
+};
+</script>
---
+++ src/views/Page404.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="notFound">
+		<img
+			src="/img/sharks/sadShark.webp"
+			alt="sad shark"
+			class="notFound__picture"
+		/>
+		<h1 class="notFound__text">
+			О нет! Мы ещё не изучили эту тему. Можем предложить перейти <router-link
+				to="/"
+			>
+				сюда
+			</router-link>
+		</h1>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.notFound {
+    width: 80vw;
+    height: fit-content;
+    margin: calc(50% - 400px) 10%;
+    display: flex;
+    align-items: center;
+
+    &__text {
+        height: fit-content;
+    }
+
+    &__picture {
+        width: 300px;
+        height: 300px;
+    }
+}
+</style>
